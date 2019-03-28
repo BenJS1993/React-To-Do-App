476d625b04e9a58cf7071a79b5eb3a69bb8a496a
Code Nation 13 <codenation@CN-13.local>
2019-03-28 11:17:50 +0000

    Updates

---
+++ precache-manifest.789aed4a2be85c120f7365f547e62f45.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4b0eea17bf36ae43eab5",
+    "url": "/React-To-Do-App/static/js/runtime~main.07775c96.js"
+  },
+  {
+    "revision": "2000d234816cae7200ac",
+    "url": "/React-To-Do-App/static/js/main.7fb38ec8.chunk.js"
+  },
+  {
+    "revision": "5883fe98142232124cba",
+    "url": "/React-To-Do-App/static/js/2.97e2f7ef.chunk.js"
+  },
+  {
+    "revision": "2000d234816cae7200ac",
+    "url": "/React-To-Do-App/static/css/main.c950edca.chunk.css"
+  },
+  {
+    "revision": "c83fbad13b3e3ec7b7b78f211779758c",
+    "url": "/React-To-Do-App/index.html"
+  }
+];
